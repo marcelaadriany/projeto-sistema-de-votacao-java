d4e602af51f8f29fb3eda5b70366efafe2d7b0ac
marcelaadriany <marcela.adriany@gmail.com>
2024-09-02 20:48:23 -0300

    requisito 2

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,9 +1,40 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * A classe PessoaCandidata representa uma pessoa candidata no sistema de votação.
+ * A classe PessoaEleitora representa uma pessoa eleitora no sistema de votação.
  */
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  // atributos
+  private int numero;
+  private int votos;
 
+  // métodos
+  // de número
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  // de votos
+  public int getVotos() {
+    return votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
+
+  /**
+   * A PessoaEleitora e seus votos.
+   */
+  // construtor
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
 }
