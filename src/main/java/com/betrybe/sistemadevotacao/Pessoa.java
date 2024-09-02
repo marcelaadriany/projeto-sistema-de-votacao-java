68825a688d0ae5675594c08cb1f6e48cfb2462fb
marcelaadriany <marcela.adriany@gmail.com>
2024-09-02 19:52:24 -0300

    requisito 1 - tentando

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,13 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  abstract
+abstract class Pessoa {
+  protected String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
